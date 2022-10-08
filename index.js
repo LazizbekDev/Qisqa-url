e3b6c78d6db8ff3537f06e7b1c3c3e9b05cf460b
LazizbekDev <lazizbektojiboyev0611@gmail.com>
2022-10-08 03:43:35 -0700

    First express app

---
+++ index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+var fs = require('fs');
+var port = 8080;
+
+app.get('/test', function (req, res) {
+    res.send('the REST endpoint test run!');
+});
+
+app.get('/', (req, res) => {
+    html = fs.readFileSync('index.html');
+    res.writeHead(200);
+    res.write(html);
+    res.end();
+})
+
+
+app.listen(port, function() {
+  console.log('Server running at http://127.0.0.1:%s', port);
+});
